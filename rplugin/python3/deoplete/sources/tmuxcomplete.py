c0e1f2b5608a38e4e2ab2895c49f98f7ddfabb65
Thales Mello <thalesmello@gmail.com>
2016-10-26 01:14:19 -0200

    System call should be done in the python process to avoid blocking Vim

--- rplugin/python3/deoplete/sources/tmuxcomplete.py
+++ rplugin/python3/deoplete/sources/tmuxcomplete.py
@@ -1,6 +1,7 @@
 from .base import Base
 
 import deoplete.util
+from subprocess import check_output
 
 class Source(Base):
     def __init__(self, vim):
@@ -12,5 +13,6 @@ class Source(Base):
         self.rank = 4
 
     def gather_candidates(self, context):
-        return [{ 'word': x } for x in
-                self.vim.call('tmuxcomplete#gather_candidates')]
+        command = self.vim.call('tmuxcomplete#getcommand', '', 'words')
+        words = check_output(['sh', '-c', command]).decode('utf-8').splitlines()
+        return [{ 'word': x } for x in words]
